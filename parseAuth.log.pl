e76469a887157653b91598162757decd2abd9009
toca2k <proxy-user@SSH-Jumphost.unterwelt.local>
2019-01-10 22:27:03 +0100

    Added first & last Date Added whois lookup for IPs Did some better formatting

--- parseAuth.log.pl
+++ parseAuth.log.pl
@@ -4,12 +4,12 @@ use Regexp::Common;
 use Net::DNS;
 use Net::Whois::IP;
 use Term::ANSIColor qw(:constants);
+use Time::ParseDate;
 use strict;
 
 my $authlogfile = "/tmp/auth.log"; 	# Where to find the logfile.
 my $hostname = "SSH-Jumphost";			# We need to know the name oft the Host where the lgofile was generated. It's needed to find specific lines.
 my $debug = 0;											# Just for debuging, if $debug >0 you will see a lot of output.
-
 
 # Open the logfile an get all Data into a Array
 open my $handle, '<', $authlogfile;
@@ -21,11 +21,24 @@ my %sshd_accepted_connections;
 my %sshd_failed_connections;
 my %sshd_bad_protocol;
 
+my $DateTimeOldestRecord = parsedate("today");
+my $DateTimeYoungestRecord = 0;
+
 # Main Loop
 foreach (@lines) {
-	my $time;
-	if ( $_ =~ /(^.*)$hostname/ ) { $time = $1; } # Timestamp is not yet used
-	
+	my $seconds_since_jan1_1970;
+	if ( $_ =~ /(^.*)$hostname/ ) { 
+		$seconds_since_jan1_1970 = parsedate($1,PREFER_PAST => 1); 
+		if ($seconds_since_jan1_1970 < $DateTimeOldestRecord) { 
+			$DateTimeOldestRecord = $seconds_since_jan1_1970; 
+
+		}
+		if ($seconds_since_jan1_1970 > $DateTimeYoungestRecord) {
+			$DateTimeYoungestRecord = $seconds_since_jan1_1970;
+		}
+
+	}
+
 	if ( $_ =~ /.*sshd.*Accepted publickey/ )		
 	{
 		if ( $_ =~ /($RE{net}{IPv4})/ )
@@ -68,15 +81,15 @@ foreach (@lines) {
 			if ($debug > 0) { print "Debug Bad proto: ", $1, " => ", $sshd_bad_protocol{$1}, "\n" ;}
 		}
 	}
-
-
 }
 
+
+print "Oldest Date   : ", UNDERLINE MAGENTA,scalar localtime( $DateTimeOldestRecord),RESET ,"\n";
+print "Youngest Date : ", UNDERLINE MAGENTA, scalar localtime( $DateTimeYoungestRecord ),RESET, "\n";
 
 my @IPs_accepted 	= keys %sshd_accepted_connections;
 my @IPs_failed   	= keys %sshd_failed_connections;
 my @IPs_bad_protocol	= keys %sshd_bad_protocol;
-
 
 print "We found ", YELLOW, $#IPs_accepted+1, RESET, " different IPs from where we ACCEPTED connections\n";
 printsummary(\%sshd_accepted_connections, "Accepted Connections", "5");
@@ -85,7 +98,7 @@ print "\nWe found ", YELLOW, $#IPs_failed+1, RESET, " different IPs from where c
 printsummary(\%sshd_failed_connections, "Failed Connections", "5");
 
 print "\nWe found ",YELLOW, $#IPs_bad_protocol+1, RESET, " different IPs who used BAD PROTOCOL\n";
-printsummary(\%sshd_bad_protocol, "Bad Protocol", "5");
+printsummary(\%sshd_bad_protocol, "Bad Protocol", "10");
 
 
 
@@ -99,12 +112,16 @@ sub printsummary {
 
 	my @tmptoptalkers = sort { $b <=> $a } values %tmphash;
 
-	print GREEN "------------------------ ", RESET, "Showing TOP-5 Talkers ", GREEN, "--------------------------", RESET, "\n";
+	print GREEN "----------------------------- ", RESET, "Showing TOP-",$tmpmaxtalkers, " Talkers ", GREEN, "-----------------------------------------", RESET, "\n";
 	for my $talker (@tmptoptalkers) {
 		if ($tmpmaxtalkers > 0) {
 			foreach (@tmpkeys) {
 				if ($tmphash{$_} eq $talker) {
-					print RED, $tmpinfotype, RESET, ": ", CYAN, $tmphash{$_}, RESET, " times from IP ", CYAN, $_, RESET, "\n";
+					my $response = whoisip_query($_);
+					if ( not length $response->{country} ) { $response->{country} = "n/a" }
+					if ( not length $response->{descr} ) { $response->{descr} = "n/a" }
+				
+					print RED, $tmpinfotype, RESET, ": ", CYAN, $tmphash{$_}, RESET, " times from IP ", CYAN, $_, RESET, "\t ( Country: ", $response->{country} ,", ", $response->{descr}  ," )\n";
 					$tmpmaxtalkers--;
 					if ( $tmpmaxtalkers <= 0) { last; }
 				} 
@@ -114,34 +131,9 @@ sub printsummary {
 			last;
 		}
 	}
-	print GREEN, "-------------------------------------------------------------------------", RESET, "\n";
+	print GREEN, "---------------------------------------------------------------------------------------------", RESET, "\n";
 }
 
 
-ip_to_dnsname("192.168.213.3");
-
-sub ip_to_dnsname {
-	my $ip_to_check = $_[0];
-	my $res = Net::DNS::Resolver->new;
-
-	# change IP from 192.168.1.15 to 15.1.168.192.in-addr.arpa for searching
-	my $target_IP = join('.', reverse split(/\./, $ip_to_check)).".in-addr.arpa";
-
-	# query DNS
-	my $query = $res->query("$target_IP", "PTR");
-
-	# if a result is found
-	if ($query){
-		print("Resolves to:\n");
-		# for every result, print the IP address
-		foreach my $rr ($query->answer){
-			# show all unless the type is PTR (pointer to a canonical name)
-			next unless $rr->type eq "PTR";
-		        # remove the period at the end
-		        printf(substr($rr->rdatastr, 0, -1));
-    		}
-	} else {
-	}
-}
 
 
